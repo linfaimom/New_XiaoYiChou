acc65239d3a936e059a9dea7855d7ba14f0024c9
marcus <linfaimom@gmail.com>
2016-06-03 22:35:54 +0800

    add a banner

--- app/src/main/java/com/example/marcus/new_xiaoyichou/BottomFragments/They.java
+++ app/src/main/java/com/example/marcus/new_xiaoyichou/BottomFragments/They.java
@@ -1,13 +1,20 @@
 package com.example.marcus.new_xiaoyichou.BottomFragments;
 
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.marcus.new_xiaoyichou.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 /**
  * Created by marcus on 16/6/2.
@@ -19,7 +26,29 @@ public class They extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.they,null);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        BGABanner autoPlayImages = (BGABanner) view.findViewById(R.id.auto_play_image);
+        // 用Java代码方式设置切换动画
+        autoPlayImages.setTransitionEffect(BGABanner.TransitionEffect.Depth);
+        // autoPlayImages.setPageTransformer(new RotatePageTransformer());
+        // 设置page切换时长
+        autoPlayImages.setPageChangeDuration(1000);
+        List<View> views = new ArrayList<>();
+        views.add(getPageView(R.drawable.image1));
+        views.add(getPageView(R.drawable.image2));
+        views.add(getPageView(R.drawable.image3));
+        autoPlayImages.setViews(views);
+    }
+
+    private View getPageView(@DrawableRes int resid) {
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageResource(resid);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        return imageView;
     }
 
     //重写setMenuVisibility方法，不然会出现叠层的现象
@@ -30,4 +59,5 @@ public class They extends Fragment {
             this.getView().setVisibility(menuVisibile ? View.VISIBLE : View.GONE);
         }
     }
+
 }
